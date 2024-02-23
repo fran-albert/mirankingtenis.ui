fbef0d460a0120fee099f4a935167dec28ddf461
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-23 19:07:58 -0300

    Add ResetPasswordPage component

---
+++ src/app/restablecer-contrase%C3%B1a/page.tsx
@@ -0,0 +1,11 @@
+import ResetPasswordForm from "@/sections/Auth/ResetPassword/ResetPasswordForm";
+
+function ResetPasswordPage() {
+  return (
+    <>
+      <ResetPasswordForm />
+    </>
+  );
+}
+
+export default ResetPasswordPage;
