d85355d8998025a6109154fb8d93fa0aa80864f8
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-23 19:07:52 -0300

    Add player page component

---
+++ src/app/jugadores/agregar/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import CreatePlayerForm from "@/sections/Players/Add/createPlayerForm";
+import React from "react";
+
+function AddPlayerPage() {
+  return (
+    <div className="flex justify-center w-full px-4 lg:px-0 mt-10">
+      <CreatePlayerForm />
+    </div>
+  );
+}
+
+export default AddPlayerPage;
