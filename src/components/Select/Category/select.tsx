3816487be2e1728b53495fd458c05a494feab54c
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-23 19:08:21 -0300

    Add CategorySelect component

---
+++ src/components/Select/Category/select.tsx
@@ -0,0 +1,52 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Category } from "@/modules/category/domain/Category";
+import { CategoryRepository } from "@/modules/category/domain/CategoryRepository";
+import { createApiCategoryRepository } from "@/modules/category/infra/ApiCategoryRepository";
+import { useEffect, useState } from "react";
+
+interface CategorySelectProps {
+  selected?: string;
+  onCategory?: (value: string) => void;
+}
+
+export const CategorySelect = ({
+  selected,
+  onCategory,
+}: CategorySelectProps) => {
+  const [category, setCategories] = useState<Category[]>([]);
+  const categoryRepository: CategoryRepository = createApiCategoryRepository();
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      try {
+        const categories = await categoryRepository.getAllCategories();
+        setCategories(categories);
+      } catch (error) {
+        console.error("Error al obtener los estados:", error);
+      }
+    };
+
+    loadCategories();
+  }, []);
+
+  return (
+    <Select value={selected} onValueChange={onCategory}>
+      <SelectTrigger className="w-full bg-gray-200 border-gray-300 text-gray-800">
+        <SelectValue placeholder="Seleccione la categoría..." />
+      </SelectTrigger>
+      <SelectContent>
+        {category.map((category) => (
+          <SelectItem key={category.id} value={String(category.id)}>
+            {category.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
