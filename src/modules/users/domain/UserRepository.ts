29fdaf0a76f5a54e8be29c15195ea2caae04df1d
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-27 21:29:24 -0300

    Refactor getUser method signature in UserRepository

--- src/modules/users/domain/UserRepository.ts
+++ src/modules/users/domain/UserRepository.ts
@@ -1,7 +1,7 @@
 import { User } from './User';
 
 export interface UserRepository {
-	getUser: (id: number, token: string) => Promise<User | undefined>;
+	getUser: (id: number) => Promise<User | undefined>;
 	getAllUsers: () => Promise<User[]>;
 	getTotalUsers: () => Promise<number>;
 	createUser: (newUser: User) => Promise<User | undefined>;
