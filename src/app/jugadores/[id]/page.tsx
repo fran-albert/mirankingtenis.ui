c74aace61e155fe565f4652ebf4516ccffc01605
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-27 21:28:52 -0300

    Add player details page

---
+++ src/app/jugadores/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import ProfilePlayer from "@/sections/Players/View/profilePlayer";
+import React from "react";
+
+function PlayerDetailsPage() {
+  return (
+    <>
+      <ProfilePlayer />
+    </>
+  );
+}
+
+export default PlayerDetailsPage;
