9de9442056c10b73783938fa85a3bcb6556f05b0
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-27 21:28:49 -0300

    Add NotFoundPage component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PiWarningCircleDuotone } from "react-icons/pi";
+
+function NotFoundPage() {
+  return (
+    <div className="container flex mt-20 px-6 py-12 mx-auto">
+      <div className="flex flex-col items-center max-w-sm mx-auto text-center">
+        <p className="p-3 text-sm font-medium text-slate-500 rounded-full bg-slate-50 dark:bg-gray-800">
+          <PiWarningCircleDuotone className="w-10 h-10" />
+        </p>
+        <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+          Página no encontrada
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default NotFoundPage;
