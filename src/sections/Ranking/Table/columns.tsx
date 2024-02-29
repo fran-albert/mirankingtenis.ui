f8493abe9833287814a251bf2db7c2ad6808f14a
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-29 02:10:08 -0300

    Refactor avatar rendering and display user initials in fallback

--- src/sections/Ranking/Table/columns.tsx
+++ src/sections/Ranking/Table/columns.tsx
@@ -35,18 +35,16 @@ ColumnDef<Ranking>[] => {
         <div className="flex items-center">
           <Avatar>
             <AvatarImage
-              // src={
-              //   row.original.photo
-              //     ? `https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/${row.original.photo}`
-              //     : "https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/default.png"
-              // }
               src={
-                "https://www.atptour.com/-/media/tennis/players/head-shot/2020/02/26/11/55/federer_head_ao20.png?sc=0&hash=7A17A4E9C10DF90A2C987081C7EEE1E8"
+                row.original.user.photo
+                  ? `https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/${row.original.user.photo}`
+                  : "https://www.atptour.com/-/media/tennis/players/head-shot/2020/02/26/11/55/federer_head_ao20.png?sc=0&hash=7A17A4E9C10DF90A2C987081C7EEE1E8"
               }
               alt="@avatar"
             />
             <AvatarFallback>
-              {`${row.original.user.lastname}${row.original.user.lastname}`}
+              {row.original.user.name.charAt(0)}
+              {row.original.user.lastname.charAt(0)}
             </AvatarFallback>
           </Avatar>
           <div className="flex flex-col ml-2">
@@ -71,9 +69,7 @@ ColumnDef<Ranking>[] => {
     },
     {
       header: "PTS",
-      cell: ({ row }) => (
-        <div className="font-bold">{row.original.points}</div>
-      ),
+      cell: ({ row }) => <div className="font-bold">{row.original.points}</div>,
     },
   ];
 
