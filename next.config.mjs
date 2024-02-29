2c49d4e57a5f21b906f90d704d831b4b98540529
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-29 02:08:37 -0300

    Add new domain to images in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.atptour.com'],
+        domains: ['www.atptour.com', 'incor-ranking.s3.us-east-1.amazonaws.com'],
     },
 };
 
