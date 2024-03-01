cde617a053fdaaca055d532e43071ec9b2163156
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-01 18:47:30 -0300

    Add toast notification for successful match deletion

--- src/sections/Matches/TennisScoreBoard/tennisScoreBoard.tsx
+++ src/sections/Matches/TennisScoreBoard/tennisScoreBoard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ScoreMatchCard } from "@/sections/Matches/ScoreMatchCard/card";
 import { Match } from "@/modules/match/domain/Match";
 import { createApiMatchRepository } from "@/modules/match/infra/ApiMatchRepository";
+import { toast } from "sonner";
 
 export const TennisScoreboard = ({
   jornada,
@@ -12,15 +13,16 @@ export const TennisScoreboard = ({
 }) => {
   const [matches, setMatches] = useState<Match[]>([]);
   const matchRepository = createApiMatchRepository();
-  const deleteMatch = matchRepository.deleteMatch;
 
   const handleDeleteMatch = async (id: number) => {
-    await deleteMatch(id);
-    // Actualizar la lista de partidos tras borrar uno.
-    const updatedMatches = await matchRepository.getByCategoryAndMatchday(idCategory, jornada);
+    await matchRepository.deleteMatch(id);
+    toast.success("Partido eliminado correctamente");
+    const updatedMatches = await matchRepository.getByCategoryAndMatchday(
+      idCategory,
+      jornada
+    );
     setMatches(updatedMatches);
   };
-
 
   useEffect(() => {
     const fetchMatches = async () => {
@@ -45,7 +47,7 @@ export const TennisScoreboard = ({
             player1={match.user1}
             player2={match.user2}
             match={match.id}
-            onDeleteMatch={() => handleDeleteMatch(match.id)} 
+            onDeleteMatch={() => handleDeleteMatch(match.id)}
           />
         ))}
       </div>
