adce47af0c627046ca5887d6cbbbce938f9d5a1f
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-01 04:12:08 -0300

    Add getByCategoryAndMatchday and deleteMatch functions to ApiMatchRepository

--- src/modules/match/infra/ApiMatchRepository.ts
+++ src/modules/match/infra/ApiMatchRepository.ts
@@ -9,7 +9,25 @@ export function createApiMatchRepository(): MatchRepository {
     return matches;
   }
 
+  async function getByCategoryAndMatchday(
+    idCategory: number,
+    matchDay: number
+  ): Promise<Match[]> {
+    const response = await axiosInstance.get(
+      `matches/by-category-and-matchday?idCategory=${idCategory}&matchday=${matchDay}`
+    );
+    const matches = response.data as Match[];
+    return matches;
+  }
+
+  async function deleteMatch(id: number): Promise<void> {
+    const response = await axiosInstance.delete(`matches/${id}`);
+    return response.data;
+  }
+
   return {
     getAllMatches,
+    getByCategoryAndMatchday,
+    deleteMatch,
   };
 }
