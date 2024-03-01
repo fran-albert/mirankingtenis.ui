a695c2ac780a8a5847573a71f5d693b9f83c6767
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-01 03:36:41 -0300

    Add RankingTabs component and update TennisScoreboard props

--- src/app/partidos/page.tsx
+++ src/app/partidos/page.tsx
@@ -2,15 +2,32 @@
 import React, { useState } from "react";
 import { TennisScoreboard } from "../../sections/Matches/TennisScoreBoard/tennisScoreBoard";
 import FixtureTabs from "@/sections/Matches/FixtureTabs/tabs";
+import RankingTabs from "@/sections/Ranking/Tabs/tabs";
 
 function MatchesPage() {
   const [selectedJornada, setSelectedJornada] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState(1);
   return (
     <>
-      <FixtureTabs onSelectJornada={(jornada) => setSelectedJornada(jornada)} />
-      <div className="flex justify-center w-full lg:px-0 m-2">
-        <div className="w-full">
-          <TennisScoreboard jornada={selectedJornada} />
+      <div className="flex justify-center w-full px-4 lg:px-0 mt-10">
+        <div className="w-full max-w-7xl space-y-4">
+          <h1 className="text-2xl text-center font-medium">Partidos</h1>
+          <RankingTabs
+            onSelectCategory={(idCategory: number) =>
+              setSelectedCategory(idCategory)
+            }
+          />
+          <FixtureTabs
+            onSelectJornada={(jornada) => setSelectedJornada(jornada)}
+          />
+          <div className="flex justify-center w-full lg:px-0 m-2">
+            <div className="w-full">
+              <TennisScoreboard
+                jornada={selectedJornada}
+                idCategory={selectedCategory}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
