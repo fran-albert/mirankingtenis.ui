785aa67b5d4cd32b2c455120bc8a64fa3a2ae822
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-01 03:36:44 -0300

    Add RankingTabs component and implement category selection

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,31 +1,23 @@
 "use client";
 import { RankingTable } from "@/sections/Ranking/Table/table";
+import RankingTabs from "@/sections/Ranking/Tabs/tabs";
 import React from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 function RankingPage() {
+  const [selectedCategory, setSelectedCategory] = React.useState(1);
   return (
     <>
       <div className="flex justify-center w-full px-4 lg:px-0 mt-10">
-        <div className="w-full max-w-7xl">
-          <div className="flex items-center justify-center py-2 px-4 bg-white rounded-lg shadow">
-            <button className="mr-2">
-              <FaArrowLeft className="h-6 w-6 text-gray-800" />
-            </button>
-
-            <div className="flex overflow-x-auto space-x-4 scrollbar-hide">
-              <span className="whitespace-nowrap py-1 px-4 bg-slate-500 text-white rounded-full">
-                Categoría A
-              </span>
-              <span className="whitespace-nowrap py-1 px-4">Categoría B</span>
-              <span className="whitespace-nowrap py-1 px-4">Categoría C</span>
-            </div>
-
-            <button className="ml-2">
-              <FaArrowRight className="h-6 w-6 text-gray-800" />
-            </button>
+        <div className="w-full max-w-7xl space-y-6">
+          <h1 className="text-2xl text-center font-medium">Ranking</h1>
+          <RankingTabs
+            onSelectCategory={(idCategory: number) =>
+              setSelectedCategory(idCategory)
+            }
+          />
+          <div className="mt-4">
+            <RankingTable selectedCategory={selectedCategory} />
           </div>
-          <RankingTable />
         </div>
       </div>
     </>
