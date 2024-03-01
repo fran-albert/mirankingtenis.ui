0348cac035d078180d554af175735bf4f746d3c6
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-01 04:12:00 -0300

    Add getByCategoryAndMatchday and deleteMatch methods to MatchRepository

--- src/modules/match/domain/MatchRepository.ts
+++ src/modules/match/domain/MatchRepository.ts
@@ -2,4 +2,6 @@ import { Match } from "./Match";
 
 export interface MatchRepository {
 	getAllMatches: () => Promise<Match[]>;
+	getByCategoryAndMatchday: (idCategory: number, matchDay: number) => Promise<Match[]>;
+	deleteMatch: (id: number) => Promise<void>;
 }
