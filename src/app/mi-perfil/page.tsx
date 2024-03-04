2b80ac88764314c1b258619eab8ba4c90bb15eb7
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-04 00:05:52 -0300

    Remove unused code and update Profile component usage

--- src/app/mi-perfil/page.tsx
+++ src/app/mi-perfil/page.tsx
@@ -1,17 +1,12 @@
-// pages/profile.js
 "use client";
-import { useCustomSession } from "@/context/SessionAuthProviders";
 import Profile from "@/sections/Auth/Profile/profile";
 
 export default function ProfilePage() {
-  const { session } = useCustomSession();
-  const idUser = session?.user.id;
-
   return (
     <>
       <div className="flex justify-center w-full px-4 lg:px-0 mt-10">
         <div className="w-full max-w-7xl space-y-6">
-          <Profile idUser={Number(idUser)} />
+          <Profile />
         </div>
       </div>
     </>
