c8afd3112b1b1b761159af1b9adc66af480e4977
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-03-08 17:26:53 -0300

    Add formatDate function to format date strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const goBack = () => {
   window.history.back();
 };
+
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${day}-${month}-${year} ${hours}:${minutes}`;
+}
