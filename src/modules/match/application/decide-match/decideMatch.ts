df9e5f7bbc2b4295d114ab397ff7dec3b88bf7af
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-06-25 16:46:59 -0300

    no message

--- src/modules/match/application/decide-match/decideMatch.ts
+++ src/modules/match/application/decide-match/decideMatch.ts
@@ -1,7 +1,7 @@
 import { MatchRepository } from "../../domain/MatchRepository";
 
 export function decideMatch(matchRepository: MatchRepository) {
-  return async (id: number, idUserWinner: number): Promise<void> => {
-    await matchRepository.decideMatch(id, idUserWinner);
+  return async (id: number, idUserWinner: number, tournament: number): Promise<void> => {
+    await matchRepository.decideMatch(id, idUserWinner, tournament);
   };
 }
