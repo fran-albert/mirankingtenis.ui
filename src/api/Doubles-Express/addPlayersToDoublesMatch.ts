cc8cacc68f45a1133516864ea7489e8eb137abf9
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2025-01-27 15:54:00 -0300

    no message

--- src/api/Doubles-Express/addPlayersToDoublesMatch.ts
+++ src/api/Doubles-Express/addPlayersToDoublesMatch.ts
@@ -1,18 +1,22 @@
-import { Match } from "@/modules/match/domain/Match";
 import axiosInstance from "@/services/axiosConfig";
-import { DoublesExhibitionMatch } from "@/types/Double-Match/DoublesExhibitionMatch";
+import { DoublesExhibitionMatchRequest } from "@/types/Double-Match/DoublesExhibitionMatch";
 
 interface IRegisterPlayer {
     playerId: number;
     slot: number
 }[]
 
-export const registerPlayerToMatch = async (matchId: number, players: IRegisterPlayer): Promise<DoublesExhibitionMatch> => {
+export const addPlayerToDoublesMatch = async (
+    matchId: number,
+    players: IRegisterPlayer
+): Promise<DoublesExhibitionMatchRequest> => {
     try {
-        const { data } = await axiosInstance.post<DoublesExhibitionMatch>(`doubles-exhibition-match/${matchId}/addPlayers`, players);
+        const { data } = await axiosInstance.post<DoublesExhibitionMatchRequest>(
+            `doubles-exhibition-match/${matchId}/add-players`,
+            players
+        );
         return data;
     } catch (error: any) {
-        // Lanzar el error de Axios correctamente para que sea capturado
         throw error || "Error desconocido";
     }
 };
