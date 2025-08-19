9cac18ac3e727f876c229843d52579743b9d0417
FRAN-PC\Francisco <fran_albert1@outlook.com>
2025-08-19 18:09:30 -0300

    feat: implement playoff data fetching with React Query; add finals, quarter-finals, and semifinals API actions; remove legacy Zustand store and PlayOffRepository

---
+++ src/api/Playoff/get-semifinals.action.ts
@@ -0,0 +1,16 @@
+import axiosInstance from "@/services/axiosConfig";
+import { GroupFixtureDto } from "@/common/types/group-fixture.dto";
+
+export async function getSemifinals(idTournament: number, idCategory: number): Promise<GroupFixtureDto[]> {
+    try {
+        const response = await axiosInstance.get(`playoff/semi-finals/${idTournament}/${idCategory}`);
+        const data = response.data as GroupFixtureDto[];
+        return data;
+    } catch (error: any) {
+        if (error?.response && error.response.status === 404) {
+            console.error("No data found for the given tournament and category.");
+            return [];
+        }
+        throw error;
+    }
+}
