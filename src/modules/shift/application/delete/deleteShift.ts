7acdece5bfc34c911f4d0cb478c81172d6dd466c
FRAN-PC\Francisco <fran_albert1@outlook.com>
2025-08-20 20:29:30 -0300

    refactor: remove Shift module and related repository functions for cleaner codebase

--- src/modules/shift/application/delete/deleteShift.ts
+++
@@ -1,7 +0,0 @@
-import { ShiftRepository } from "../../domain/ShiftRepository";
-
-export function deleteShift(shiftRepository: ShiftRepository) {
-  return async (id: number): Promise<void> => {
-    await shiftRepository.deleteShift(id);
-  };
-}
--- src/modules/shift/application/shift-for-match/shiftForMatch.ts
+++
@@ -1,8 +0,0 @@
-import { Shift } from "../../domain/Shift";
-import { ShiftRepository } from "../../domain/ShiftRepository";
-
-export function shiftForMatch(shiftRepository: ShiftRepository) {
-  return async (shift: Shift, idMatch: number): Promise<Shift | undefined> => {
-    return await shiftRepository.shiftForMatch(shift, idMatch);
-  };
-}
