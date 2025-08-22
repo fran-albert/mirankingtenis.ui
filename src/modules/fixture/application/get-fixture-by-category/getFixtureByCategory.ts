5539ecf85c0298952dd2a4c8856b5d5faeff463a
FRAN-PC\Francisco <fran_albert1@outlook.com>
2025-08-22 13:12:59 -0300

    refactor: remove unused fixture-related code and interfaces

--- src/modules/fixture/application/get-fixture-by-category/getFixtureByCategory.ts
+++
@@ -1,8 +0,0 @@
-import { Fixture } from "../../domain/Fixture";
-import { FixtureRepository } from "../../domain/FixtureRepository";
-
-export function getFixtureByCategory(fixtureRepository: FixtureRepository) {
-  return async (idCategory: number): Promise<Fixture[]> => {
-    return await fixtureRepository.getFixtureByCategory(idCategory);
-  };
-}
