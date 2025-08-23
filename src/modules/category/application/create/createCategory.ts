41f6a64a8545dc63d39bdb8a3a905bf398e150af
FRAN-PC\Francisco <fran_albert1@outlook.com>
2025-08-22 21:13:49 -0300

    refactor: remove category management code and related hooks

--- src/modules/category/application/create/createCategory.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from "../../domain/Category";
-import { CategoryRepository } from "../../domain/CategoryRepository";
-
-export function createCategory(categoryRepository: CategoryRepository) {
-    return async (newCategory: Category): Promise<Category | undefined> => {
-        return await categoryRepository.createCategory(newCategory);
-    };
-}
