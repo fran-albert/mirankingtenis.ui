aac3bb22f045663a26ea7181a5456eb81c92be35
FRAN-PC\Francisco <fran_albert1@outlook.com>
2025-08-22 21:50:50 -0300

    refactor: remove deprecated ranking-related files and interfaces

--- src/modules/ranking/application/get-by-category/getRankingByCategory.ts
+++
@@ -1,8 +0,0 @@
-import { Ranking } from "../../domain/Ranking";
-import { RankingRepository } from "../../domain/RankingRepository";
-
-export function getRankingByCategory(rankingRepository: RankingRepository) {
-  return async (idCategory: number): Promise<Ranking[]> => {
-    return await rankingRepository.getRankingByCategory(idCategory);
-  };
-}
--- src/modules/ranking/application/get/getRanking.ts
+++
@@ -1,8 +0,0 @@
-import { Ranking } from "../../domain/Ranking";
-import { RankingRepository } from "../../domain/RankingRepository";
-
-export function get(rankingRepository: RankingRepository) {
-  return async (): Promise<Ranking[]> => {
-    return await rankingRepository.get();
-  };
-}
